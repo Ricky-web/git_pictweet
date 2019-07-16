e36c51cdbf5f5c28993807ae745c8903b14c4c19
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-07-16 11:24:38 +0000

    test~10examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,35 @@ describe User do
       another_user.valid?
       expect(another_user.errors[:email]).to include("has already been taken")
     end
+    it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+    it "is invalid without a password_confirmation" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+    it "is invalid with a nickname that has more than 7 characters" do
+      user = build(:user, nickname: "1234567")
+      user.valid?
+      expect(user.errors[:nickname][0]).to include("is too long (maximum is 6 characters)")
+    end
+    it "is valid with a nickname that has less than 6 characters" do
+      user = build(:user, nickname: "123456")
+      user.valid?
+      expect(user).to be_valid
+    end
+    it "is vaild with a password that has more than 6 characters" do
+      user = build(:user, password: "123456", password_confirmation: "123456")
+      user.valid?
+      expect(user).to be_valid
+    end
+    it "is invalid with a password that has less than 5 characters" do
+      user = build(:user, password: "12345", password_confirmation: "12345")
+      user.valid?
+      expect(user.errors[:password][0]).to include("is too short (minimum is 6 characters)")
+    end
   end
 end
