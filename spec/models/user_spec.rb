8e5b773c1836d386e80814830fcef92dd21cf617
EC2 Default User <ec2-user@ip-172-31-20-222.ap-southeast-1.compute.internal>
2019-07-16 10:51:55 +0000

    test~4examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
     it "is invalid without an email" do
-      user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
+    end
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user). to be_valid
+    end
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
     end
   end
 end
